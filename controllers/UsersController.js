750c0262b83d92c14159a93e80ec6e7957231ef3
George Emetu <g.emetu11@gmail.com>
2024-03-12 15:33:01 +0100

    adding a user

---
+++ controllers/UsersController.js
@@ -0,0 +1,43 @@
+import dbClient from '../utils/db';
+import sha1 from 'sha1';
+import Queue from 'bull';
+
+const userQueue = new Queue('userQueue');
+
+export default class UsersController {
+  static postuser(req, res) {
+    /** checks if req.body exists, if yes, assigns the value of req.body.email
+     *  to the variable email, else':' assign null
+     */
+    const email = req.body ? req.body.email : null;
+    const password = req.body ? req.body.password : null;
+
+    if (!email) {
+      res.status(400).json({email : 'Missing email'});
+      return;
+    }
+
+    if (!password) {
+      res.status(400).json({ error: 'Missing password' });
+      return;
+    }
+
+    const users = dbClient.db.collection('users');
+    users.findOne({ email }, (err, user) => {
+      if (user) {
+        response.status(400).json({ error: 'Already exist' });
+      } else {
+        const hashedPassword = sha1(password);
+        users.insertOne(
+          {
+            email,
+            password: hashedPassword,
+          },
+        ).then((result) => {
+          response.status(201).json({ id: result.insertedId, email });
+          userQueue.add({ userId: result.insertedId });
+        }).catch((error) => console.log(error));
+      }
+    });
+  }
+}
